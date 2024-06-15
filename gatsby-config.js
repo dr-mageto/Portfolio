16496acce72538e3a73b84bb0e04ef1aa054cc47
dr-mageto <derryk.em@gmail.com>
2024-06-15 21:33:07 +0300

    Tweaked About Section allow empty

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Derrick Mageto',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'As a data analyst, I have specialized in interpreting and visualizing complex data sets to deliver key insights.',
+    siteUrl: 'https://derrickmageto.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    // twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Derrick Mageto',
+        short_name: 'Derrick Mageto',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
