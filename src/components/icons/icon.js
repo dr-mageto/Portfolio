2c8c5943ae2c1e3bfd242af3923c3c6b5f2a1564
dr-mageto <derryk.em@gmail.com>
2024-06-17 18:50:27 +0300

    Tweaked About Section allow empty

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -15,6 +15,7 @@ import {
   IconPlayStore,
   IconStar,
   IconTwitter,
+  IconWhatsApp,
 } from '@components/icons';
 
 const Icon = ({ name }) => {
@@ -47,6 +48,8 @@ const Icon = ({ name }) => {
       return <IconStar />;
     case 'Twitter':
       return <IconTwitter />;
+    case 'WhatsApp':
+      return <IconWhatsApp />;
     default:
       return <IconExternal />;
   }
